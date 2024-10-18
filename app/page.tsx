662bed14342532577f80135937300c686ec52694
fancyhorse <qraima1@gmail.com>
2024-10-18 05:39:58 -0700

    Fixed linting errors and updated files for Vercel deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,10 @@
 
-import Image from "next/image";
-
 export default function Home() {
   return (
     <div>
 
       <h1>it's Raima</h1>
-      <img src="https://cdn.sanity.io/images/lqlx0enf/production/430fec9dadb646f6089bdafd0d06e701e828a4e9-855x392.png" height="100%" width="100%"></img>
+      <img src="https://cdn.sanity.io/images/lqlx0enf/production/430fec9dadb646f6089bdafd0d06e701e828a4e9-855x392.png" alt="next.js image" height="100%" width="100%"></img>
 
     </div>
   );
