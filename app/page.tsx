3ad46850cc5f9ca5c9c065213d2529e340433390
fancyhorse <qraima1@gmail.com>
2024-10-18 06:09:46 -0700

    Fixed linting errors and updated files for Vercel deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,16 @@
+import Image from 'next/image';
 
 export default function Home() {
   return (
     <div>
-
       <h1>it's Raima</h1>
-      <img src="https://cdn.sanity.io/images/lqlx0enf/production/430fec9dadb646f6089bdafd0d06e701e828a4e9-855x392.png" alt="next.js image" height="100%" width="100%"></img>
-
+      <Image 
+        src="https://cdn.sanity.io/images/lqlx0enf/production/430fec9dadb646f6089bdafd0d06e701e828a4e9-855x392.png" 
+        alt="next.js image" 
+        width={855} // Width in pixels
+        height={392} // Height in pixels for proper aspect ratio
+        layout="responsive" // Use layout responsive if you want to keep the aspect ratio
+      />
     </div>
   );
 }
--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,10 @@
+// next.config.mjs
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
+const nextConfig = {
+    images: {
+      domains: ['cdn.sanity.io'], // Allow this domain for images
+    },
+  };
+  
+  export default nextConfig; // Use export default instead of module.exports
+  
